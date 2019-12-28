2dc52faf5fa694c68099a9da6dacbf6c86a01a6b
fansUI <50223100+fansUI@users.noreply.github.com>
2019-12-28 22:10:46 +0800

    代码优化

--- 002.antd-redux-todolist/src/store/reducer.js
+++ 002.antd-redux-todolist/src/store/reducer.js
@@ -6,19 +6,20 @@ let defaultState = {
 }
 
 export default (state=defaultState, action)=>{
+    let newState = JSON.parse(JSON.stringify(state))  // 把这句话提到这里，可以少写几行代码
     if(action.type==='change_input_value'){
-        let newState = JSON.parse(JSON.stringify(state))
+        // let newState = JSON.parse(JSON.stringify(state))
         newState.inputValue = action.value
         return newState
     }
     if(action.type==='add_todo_item'){
-        let newState = JSON.parse(JSON.stringify(state))
+        // let newState = JSON.parse(JSON.stringify(state))
         newState.lists.push(newState.inputValue)
         newState.inputValue = ''
         return newState
     }
     if(action.type==='del_todo_item'){
-        let newState = JSON.parse(JSON.stringify(state))
+        // let newState = JSON.parse(JSON.stringify(state))
         newState.lists.splice(action.index,1)
         return newState
     }
